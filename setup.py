a5d563d9a8b69ac3854f604026cbb22cf2b2b776
fgiuba <federico.giuba@copangroup.com>
2019-06-06 11:26:10 +0200

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ipconflict',
-    version='0.3.0',
+    version='0.3.1',
     description='Check for conflicts between network subnets',
     long_description=long_description,
     long_description_content_type="text/markdown",
